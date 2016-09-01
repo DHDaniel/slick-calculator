8c4245db9af2ddcbf7fafd0a958e0cbf7209f1da
Daniel Hernandez <dh.danny@hotmail.com>
2016-09-01 19:52:19 -0400

    Fully functioning calculator, however does not check for overflow of numbers from container

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
 var $input = $(".input-field");
 var $chain = $(".chain");
 
+// initialising result as global
+var result;
+
 // clearing and retrieving past input
 function updateInput() {
   var val = $.trim($input.html());
@@ -16,12 +19,15 @@ function updateChain(val) {
     $chain.append(val);
   }
 }
+
+// clears everything
 function ac() {
   $input.html(0);
   $chain.html(0);
   currentOperation = null;
 }
 
+// clears last entry and operation
 function ce() {
   $input.html(0);
   var chainStr = $.trim($chain.html());
@@ -42,6 +48,11 @@ var currentOperation = null;
 // digits
 $(".numbers td").click(function () {
 
+  // clearing everything if we have already produced a result and are typing a number directly after
+  if (currentOperation == "result") {
+   ac();
+  }
+
   if (currentOperation != "number") {
     updateInput();
     currentOperation = "number";
@@ -55,9 +66,15 @@ $(".numbers td").click(function () {
 // operations
 $("#calculations td").click(function () {
 
+  var operation = $.trim($(this).html());
+
+  // making new chain if we are entering an operation after getting a result
+  if (currentOperation == "result") {
+    $chain.html(result);
+  }
+
   currentOperation = "calculation";
 
-  var operation = $.trim($(this).html());
   updateChain(operation);
 
 });
@@ -73,6 +90,68 @@ $("#clear td").click(function () {
     case "CE":
       ce();
       break;
+  }
+});
+
+$("#equals").click(function () {
+
+  if (currentOperation != "result") {
+
+    currentOperation = "result";
+
+    var chainStr = $.trim($chain.html());
+    var getNums = /[^\+|\-|÷|×|=]+/g;
+    var getOps = /[\+|\-|÷|×]/g;
+
+    var numbers = [];
+    var operations = [];
+
+    do {
+      var match = getNums.exec(chainStr);
+      if (match) {
+        numbers.push(parseFloat(match[0]));
+      }
+    } while (match)
+
+    do {
+      var match = getOps.exec(chainStr);
+      if (match) {
+        operations.push(match[0]);
+      }
+    } while (match)
+
+    result = numbers[0];
+    var resultStr = result;
+
+    for (var i = 0; i < operations.length; i++) {
+
+      // applying operation to number that follows
+      switch (operations[i]) {
+        case "+":
+          resultStr += '+' + numbers[i + 1];
+          break;
+        case "-":
+          resultStr += '-' + numbers[i + 1];
+          break;
+        case "÷":
+          resultStr += '/' + numbers[i + 1];
+          break;
+        case "×":
+          resultStr += '*' + numbers[i + 1];
+          break;
+      }
+
+    }
+
+    // easiest way to keep BIDMAS order
+    result = eval(resultStr);
+
+    if (result) {
+      $input.html(result);
+      $chain.append(result);
+    } else {
+      $input.html("Error");
+    }
   }
 
 });
